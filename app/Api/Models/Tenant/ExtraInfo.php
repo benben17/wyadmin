cdd373763afc5bb8a463cb7be6b925f282f2b6ee
892100089 <892100089@qq.com>
2021-07-10 23:38:29 +0800

    bill

--- app/Api/Models/Tenant/ExtraInfo.php
+++ app/Api/Models/Tenant/ExtraInfo.php
@@ -15,7 +15,7 @@ class ExtraInfo extends Model
    */
 
   protected $table = 'bse_tenant_extra';
-  protected $fillable = ['cus_id', 'demand_area', 'expected_price', 'trim_state', 'recommend_room_id', 'recommend_room', 'purpose_room', 'purpose_price', 'purpose_term_lease', 'purpose_free_time', 'current_proj', 'current_addr', 'current_area', 'current_price', 'c_uid', 'u_uid'];
+  protected $fillable = ['tenant_id', 'demand_area', 'expected_price', 'trim_state', 'recommend_room_id', 'recommend_room', 'purpose_room', 'purpose_price', 'purpose_term_lease', 'purpose_free_time', 'current_proj', 'current_addr', 'current_area', 'current_price', 'c_uid', 'u_uid'];
   protected $hidden = ['deleted_at', 'c_uid', 'u_uid', 'updated_at', 'created_at'];
 
   public function tenant()
