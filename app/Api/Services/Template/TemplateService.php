d562983030d2948fbc882200b18c1ee772cc823f
892100089 <892100089@qq.com>
2021-07-14 17:40:48 +0800

    fix

--- app/Api/Services/Template/TemplateService.php
+++ app/Api/Services/Template/TemplateService.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Api\Services\Template;
 
 use Illuminate\Support\Facades\Log;
@@ -10,6 +11,7 @@ use PhpOffice\PhpWord\Writer\Html as WriteHtml;
 use PhpOffice\PhpWord\TemplateProcessor;
 
 use App\Api\Models\Company\Template as TemplateModel;
+
 /**
  *合同管理
  */
@@ -23,30 +25,31 @@ class TemplateService
      * @DateTime 2020-06-14
      * @param    [type]     $parm     [模版文件、保存文件]
      * @param    [type]     $contract [合同信息]
-         * @return   [bool]               [true or false]
+     * @return   [bool]               [true or false]
      */
-    public function exportContract($parm,$contract){
-        $data['租户名称'] = $contract['customer_name'];
+    public function exportContract($parm, $contract)
+    {
+        $data['租户名称'] = $contract['name'];
         $data['合同编号'] = $contract['contract_no'];
         $data['租金单价'] = $contract['rental_price'];
         $data['租金月金额'] = $contract['rental_month_amount'];
         $data['租金月金额大写'] = amountToCny(numFormat($contract['rental_month_amount']));
-        $data['签约年月日'] = dateFormat("Y年m月d日",$contract['sign_date']);
+        $data['签约年月日'] = dateFormat("Y年m月d日", $contract['sign_date']);
         $data['付费方式'] = $contract['pay_method'];
         $data['管理费单价'] = $contract['management_price'];
         $data['管理费月金额'] = $contract['management_month_amount'];
         $data['管理费月金额大写'] = amountToCny(numFormat($contract['management_month_amount']));
-        $data['租赁开始年月日'] = dateFormat("Y年m月d日",$contract['start_date']);
-        $data['租赁结束年月日'] = dateFormat("Y年m月d日",$contract['end_date']);
-        $data['押金收款年月日'] = dateFormat("Y年m月d日",$contract['sign_date']);
+        $data['租赁开始年月日'] = dateFormat("Y年m月d日", $contract['start_date']);
+        $data['租赁结束年月日'] = dateFormat("Y年m月d日", $contract['end_date']);
+        $data['押金收款年月日'] = dateFormat("Y年m月d日", $contract['sign_date']);
         $data['租赁周期月'] = $contract['lease_term'];
-        $data['租赁周期年'] = numFormat($contract['lease_term']/12);
+        $data['租赁周期年'] = numFormat($contract['lease_term'] / 12);
         $data['租户法人'] = $contract['customer_legal_person'];
         $data['租户签约人'] = $contract['customer_sign_person'];
         $data['计租面积'] = $contract['sign_area'];
         $data['租赁押金金额'] = numFormat($contract['rental_deposit_amount']);
         $data['管理押金金额'] = numFormat($contract['manager_deposit_amount']);
-        $data['押金收款日'] = dateFormat("Y年m月d日",$contract['sign_date']);
+        $data['押金收款日'] = dateFormat("Y年m月d日", $contract['sign_date']);
         $data['提前几个月付款'] = $contract['ahead_pay_month'];
         $data['管理费收款账户'] = $contract['manager_account_name'];
         $data['管理费收款账户银行'] = $contract['manager_bank_name'];
@@ -63,7 +66,7 @@ class TemplateService
                 $rooms .= $v['room_no'];
             }
             $data['房源楼栋'] = $buildInfo;
-            $data['楼层房号'] = $floorInfo."-".$rooms;
+            $data['楼层房号'] = $floorInfo . "-" . $rooms;
         }
 
 
@@ -72,15 +75,14 @@ class TemplateService
             $freePeriod = "";
             foreach ($contract['free_list'] as $k => $v) {
                 $num += $v['free_num'];
-                $freePeriod .='自'.dateFormat("Y年m月d日",$v['start_date']).'起至'.dateFormat("Y年m月d日",$v['start_date']).'结束；';
+                $freePeriod .= '自' . dateFormat("Y年m月d日", $v['start_date']) . '起至' . dateFormat("Y年m月d日", $v['start_date']) . '结束；';
             }
-            if ($contract['free_type'] ==1) {
+            if ($contract['free_type'] == 1) {
                 $data['免租类型'] = '月';
                 $data['免租金额'] = numFormat($num * $contract['rental_month_amount']);
-            }else{
+            } else {
                 $data['免租类型'] = '天';
                 $data['免租金额'] = numFormat($num * $contract['rental_price']);
-
             }
             $data['租赁免租期'] = $num;
             $data['免租时间段'] = $freePeriod;
@@ -90,14 +92,14 @@ class TemplateService
             $template->setValues($data);
 
             if (!is_dir($parm['savePath'])) {
-                mkdir($parm['savePath'],0755,true);
+                mkdir($parm['savePath'], 0755, true);
             }
-            $saveFile = $parm['savePath'].$parm['fileName'];
+            $saveFile = $parm['savePath'] . $parm['fileName'];
             $template->saveAs($saveFile);
 
             return true;
         } catch (Exception $e) {
-            Log::error('模版转换'.$e->getMessage());
+            Log::error('模版转换' . $e->getMessage());
             return false;
         }
     }
@@ -110,7 +112,8 @@ class TemplateService
      * @param    [type]     $id [description]
      * @return   [type]         [description]
      */
-    public function getTemplate($id){
+    public function getTemplate($id)
+    {
         $template = TemplateModel::find($id);
         return $template;
     }
@@ -123,12 +126,13 @@ class TemplateService
      * @param    [type]     $user [description]
      * @return   [type]           [description]
      */
-    public function saveTemplate($DA,$user){
+    public function saveTemplate($DA, $user)
+    {
 
-        if (isset($DA['id']) && $DA['id']>0) {
+        if (isset($DA['id']) && $DA['id'] > 0) {
             $template = TemplateModel::find($DA['id']);
             $template['u_uid'] = $user->id;
-        }else{
+        } else {
             $template = new TemplateModel;
             $template->c_uid = $user->id;
             $template->company_id = $user->company_id;
@@ -136,12 +140,11 @@ class TemplateService
             $template->type = $DA['type'];
         }
         $template->name = $DA['name'];
-        $template->file_name = isset($DA['file_name']) ? $DA['file_name'] :"";
+        $template->file_name = isset($DA['file_name']) ? $DA['file_name'] : "";
         $template->file_path = $DA['file_path'];
 
-        $template->remark = isset($DA['remark']) ?$DA['remark'] :"";
+        $template->remark = isset($DA['remark']) ? $DA['remark'] : "";
         $res = $template->save();
         return $res;
     }
 }
-
