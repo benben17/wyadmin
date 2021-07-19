839c6375357cc258a634b41bd56280f51c80c275
892100089 <892100089@qq.com>
2021-07-19 10:44:24 +0800

    meter fix

--- app/Api/Controllers/Operation/TenantController.php
+++ app/Api/Controllers/Operation/TenantController.php
@@ -147,7 +147,7 @@ class TenantController extends BaseController
         if ($checkRepeat) {
             return $this->error('客户名称重复!');
         }
-        $DA['type'] = AppEnum::TenantType;
+        $DA['type'] = AppEnum::Customer;
         try {
             DB::transaction(function () use ($DA) {
                 $res = $this->tenantService->saveTenant($DA, $this->user);
--- app/Enums/AppEnum.php
+++ app/Enums/AppEnum.php
@@ -8,6 +8,7 @@ use MyCLabs\Enum\Enum;
 final class AppEnum extends Enum
 {
   const Channel       = 1;  //  渠道
+  const Customer      = 2;
   const Supplier      = 3;  //  供应商
   const Relationship  = 4;  // 公共关系
   const Tenant        = 5;  // 租户
@@ -27,21 +28,20 @@ final class AppEnum extends Enum
   const workorderClose    = 4;   // 关闭
   const workorderRate     = 5;   //评价
 
-  const rentFeeType       = 101;    // 租金规则
-  const waterFeeType      =   103;
-  const electricFeeType      =   104;
+  const rentFeeType       =   101;    // 租金类型id
+  const waterFeeType      =   103;    // 水类型id
+  const electricFeeType   =   104;     // 电类型
 
-  const TenantType = 2;
-
+  const TenantType = 2;  // 客户类型  1 客户  2 租户
   const feeType  = 1;
   const depositFeeType = 2; // 押金类型
 
 
   // charge
-  const chargeIncome = 1;  // 收入
+  const chargeIncome = 1;  // 收入     
   const chargePay  = 2;    // 支出
 
 
-  const chargeVerify  = 1;
+  const chargeVerify  = 1;  // 
   const chargeUnVerify  = 0;
 }
