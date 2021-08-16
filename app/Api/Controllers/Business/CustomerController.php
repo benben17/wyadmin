5199041906eba556724189203f361e3cdb9eef05
892100089 <892100089@qq.com>
2021-08-16 14:20:58 +0800

    bug fix

--- app/Api/Controllers/Business/CustomerController.php
+++ app/Api/Controllers/Business/CustomerController.php
@@ -108,7 +108,7 @@ class CustomerController extends BaseController
             })
             ->with('contacts')
             ->with('extraInfo')
-            ->where('extraInfo', function ($q) use ($request) {
+            ->whereHas('extraInfo', function ($q) use ($request) {
                 $request->demand_area && $q->where('demand_area', $request->demand_area);
             })
             // ->with('customerRoom')
