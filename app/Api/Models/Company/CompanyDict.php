f008233ef9fe952aa17293e0333c7a8585551e8c
892100089 <892100089@qq.com>
2021-08-27 15:11:47 +0800

    tenant

--- app/Api/Models/Company/CompanyDict.php
+++ app/Api/Models/Company/CompanyDict.php
@@ -18,7 +18,14 @@ class CompanyDict extends Model
 	protected $hidden = ['created_at', 'updated_at', 'c_uid', 'u_uid'];
 
 	protected $fillable = ['company_id', 'dict_key', 'dict_value', 'c_uid', 'u_uid', 'is_vaild'];
+	protected $appends = ['is_vaild_label'];
 
+	public function getIsVaildLabelAttribute()
+	{
+		if (isset($this->attributes['fee_type'])) {
+			return $this->attributes['fee_type'] ? "启用" : "禁用";
+		}
+	}
 	public function extra()
 	{
 		return $this->hasMany('App\Api\Models\Channel\Channel', 'channel_type', 'dict_value');
