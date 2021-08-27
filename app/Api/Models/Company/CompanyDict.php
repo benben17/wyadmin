d81b39faa6504f4a438eaf925be1b4f6de749e2d
892100089 <892100089@qq.com>
2021-08-27 14:49:23 +0800

    tenant

--- app/Api/Models/Company/CompanyDict.php
+++ app/Api/Models/Company/CompanyDict.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Api\Models\Company;
 
 use Illuminate\Database\Eloquent\Model;
@@ -7,20 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class CompanyDict extends Model
 {
 	/**
-	* 关联到模型的数据表
-	*
-	* @var string
-	*/
+	 * 关联到模型的数据表
+	 *
+	 * @var string
+	 */
 	// use SoftDeletes;
 	protected $table = 'bse_dict';
 
-	protected $hidden = ['created_at','company_id','updated_at','c_uid','u_uid','is_vaild'];
+	protected $hidden = ['created_at', 'updated_at', 'c_uid', 'u_uid', 'is_vaild'];
 
-	protected $fillable = ['company_id','dict_key','dict_value','c_uid','u_uid','is_vaild'];
+	protected $fillable = ['company_id', 'dict_key', 'dict_value', 'c_uid', 'u_uid', 'is_vaild'];
 
 	public function extra()
 	{
-		return $this->hasMany('App\Api\Models\Channel\Channel','channel_type','dict_value');
+		return $this->hasMany('App\Api\Models\Channel\Channel', 'channel_type', 'dict_value');
 	}
-
 }
