2cf44ebfe35ec6d73f4b82cf507b8be0c438fe80
leezhua17 <892100089@qq.com>
2024-03-04 11:22:12 +0800

    plan

--- app/Api/Controllers/Business/ChannelController.php
+++ app/Api/Controllers/Business/ChannelController.php
@@ -119,7 +119,7 @@ class ChannelController extends BaseController
                 $request->end_time && $q->where('created_at', '<=', $request->end_time);
             })
             ->with([
-                'channelPolicy:id,name,channel_type',
+                'channelPolicy:id,name',
                 'channelContact',
             ])
             ->withCount('customer')
