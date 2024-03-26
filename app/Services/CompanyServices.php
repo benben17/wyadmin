708b27c51aa938cdd28da61674951ed7dbb2e06c
leezhua17 <892100089@qq.com>
2024-03-26 14:35:31 +0800

    companyservice

--- app/Services/CompanyServices.php
+++ app/Services/CompanyServices.php
@@ -1,16 +1,8 @@
 <?php
-<<<<<<< HEAD
 namespace App\Services;
 use App\Models\Company as CompanyModel;
 use App\Api\Models\Project;
-=======
 
-namespace App\Services;
-
-use App\Models\Company as CompanyModel;
-use App\Api\Models\Project;
-
->>>>>>> 7a1f975c4e970e19a897d9b5e27f84a33481477b
 /**
  *
  */
@@ -32,19 +24,9 @@ class CompanyServices
    */
   public function checkProjCount($Id)
   {
-<<<<<<< HEAD
-    $projCount = Project::where('is_vaild',1)->count();
-    $companyInfo = $this->getCompanyById($Id);
-    if ($projCount < $companyInfo['proj_count']) {
-      return false;
-    } else {
-      return true;
-    }
-=======
     $projCount = Project::where('is_valid', 1)->count();
     $companyInfo = $this->getCompanyById($Id);
     return $projCount < $companyInfo['proj_count'] ? false : true;
->>>>>>> 7a1f975c4e970e19a897d9b5e27f84a33481477b
   }
   /**
    * 获取项目名称
@@ -53,18 +35,9 @@ class CompanyServices
    * @param    [type]     $projId [项目ID]
    * @return   [String]             [项目名称]
    */
-<<<<<<< HEAD
   public function getProjName($projId){
     $proj = Project::select('proj_name')->find($projId);
     return $proj['proj_name'];
   }
 
 }
-=======
-  public function getProjName($projId)
-  {
-    $proj = Project::select('proj_name')->find($projId);
-    return $proj['proj_name'];
-  }
-}
->>>>>>> 7a1f975c4e970e19a897d9b5e27f84a33481477b
