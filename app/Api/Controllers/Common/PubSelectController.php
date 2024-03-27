24dbe39ef8f4c52991849a8864b224ee46736fd5
leezhua17 <892100089@qq.com>
2024-03-27 21:49:30 +0800

    fix

--- app/Api/Controllers/Common/PubSelectController.php
+++ app/Api/Controllers/Common/PubSelectController.php
@@ -522,14 +522,14 @@ class PubSelectController extends BaseController
 
 	public function tenantList(Request $request)
 	{
-		if (!is_array($request->proj_ids)) {
-			$request->proj_ids = str2Array($request->proj_ids);
-		}
+		$request->validate([
+			'proj_ids' => 'required',
+		]);
 
 		$data = \App\Api\Models\Tenant\Tenant::select('id', 'name', 'industry', 'level', 'proj_id', 'on_rent', 'state')
 			->where(function ($q) use ($request) {
 				// $q->where('parent_id', 0);
-				$request->proj_ids && $q->whereIn('proj_id', $request->proj_ids);
+				$request->proj_ids && $q->whereIn('proj_id', str2Array($request->proj_ids));
 				if ($request->type == 1) {
 					$q->where('type', "!=", AppEnum::TenantType);
 					if (!$this->user['is_admin']) {
