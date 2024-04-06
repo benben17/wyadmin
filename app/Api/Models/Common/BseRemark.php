31b88c29cff5b27e6a5ff228d0fb59290cbee69c
leezhua17 <leezhenhua17@gmail.com>
2024-04-06 12:39:43 +0800

    bug fix

--- app/Api/Models/Common/BseRemark.php
+++ app/Api/Models/Common/BseRemark.php
@@ -2,26 +2,33 @@
 
 namespace App\Api\Models\Common;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Api\Scopes\CompanyScope;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BseRemark extends Model
 {
   /**
+   * 备注公共模型
    *
+   * @Author leezhua
+   * @DateTime 2024-03-28
    */
   use SoftDeletes;
   protected $table = 'bse_common_remark';
   protected $hidden = ['deleted_at', "company_id", 'c_uid', 'u_uid', 'updated_at', 'company_id', 'id'];
   protected $fillable = ['company_id', 'parent_type', 'parent_id', 'remark_content', 'c_user', 'c_uid', 'u_uid'];
-
+  protected $appends = ['c_user_name'];
 
   public function addAll(array $data)
   {
-    $res = DB::table($this->getTable())->insert($data);
-    return $res;
+    return $this->insert($data);
+  }
+
+  public function getCUserNameAttribute()
+  {
+    return $this->createUser->realname;
   }
 
   public function createUser()
