ba259af033587e874cfd95d964315d581ef6a1aa
leezhua17 <leezhenhua17@gmail.com>
2024-04-27 14:58:53 +0800

    bug fix

--- app/Api/Services/Contract/BillRuleService.php
+++ app/Api/Services/Contract/BillRuleService.php
@@ -81,26 +81,30 @@ class BillRuleService
         $data[$k]['u_uid']       = $user['id'];
         $data[$k]['tenant_id']   = $tenantId;
         $data[$k]['contract_id'] = $contractId;
-        $data[$k]['type']        = $v['type'] ?? 1;
+        $data[$k]['type']        = $v['type'];
         $data[$k]['fee_type']    = $v['fee_type'];
-        $data[$k]['unit_price']  = $v['unit_price'];
+        $data[$k]['unit_price']  = $v['unit_price'] ?? 0.00;
         $data[$k]['price_type']  = $v['price_type'] ?? 1;
         $data[$k]['start_date']  = $v['start_date'] ?? "";
-        $data[$k]['bill_type']        = $v['bill_type'] ?? 1;
-        $data[$k]['end_date']         = $v['end_date'] ?? "";
-        $data[$k]['charge_date']      = $v['charge_date'] ?? "";
-        $data[$k]['area_num']         = $v['area_num'] ?? 0.00;
-        $data[$k]['lease_term']       = $v['lease_term'] ?? 0;
-        $data[$k]['pay_method']       = isset($v['pay_method']) ? $v['pay_method'] : 0;
-        $data[$k]['bill_day']         = isset($v['bill_day']) ? $v['bill_day'] : 0;
-        $data[$k]['amount']           = isset($v['amount']) ? $v['amount'] : 0.00;
-        $data[$k]['month_amt']        = isset($v['month_amt']) ? $v['month_amt'] : 0.00;
+        $data[$k]['bill_type']   = $v['bill_type'] ?? 1;
+        $data[$k]['end_date']    = $v['end_date'] ?? "";
+        $data[$k]['charge_date'] = $v['charge_date'] ?? "";
+        $data[$k]['area_num']    = $v['area_num'] ?? 0.00;
+        $data[$k]['lease_term']  = $v['lease_term'] ?? 0;
+        $data[$k]['pay_method']  = isset($v['pay_method']) ? $v['pay_method'] : 0;
+        $data[$k]['bill_day']    = isset($v['bill_day']) ? $v['bill_day'] : 0;
+        $data[$k]['amount']      = isset($v['amount']) ? $v['amount'] : 0.00;
+        $data[$k]['month_amt']   = isset($v['month_amt']) ? $v['month_amt'] : 0.00;
         $data[$k]['ahead_pay_month']  = isset($v['ahead_pay_month']) ? $v['ahead_pay_month'] : 0;
         $data[$k]['increase_show']    = $v['increase_show'] ?? 0;
         $data[$k]['increase_rate']    = $v['increase_rate'] ?? 0;
         $data[$k]['increase_start_period'] = $v['increase_start_period'] ?? 0;
         $data[$k]['increase_date']    = $v['increase_date'] ?? null;
-        $data[$k]['unit_price_label'] = $v['price_type'] == 1 ? AppEnum::dayPrice : AppEnum::monthPrice;
+        if (isset($v['price_type'])) {
+          $data[$k]['unit_price_label'] = $v['price_type'] == 1 ? AppEnum::dayPrice : AppEnum::monthPrice;
+        } else {
+          $data[$k]['unit_price_label'] = "";
+        }
         $data[$k]['remark']           = $v['remark'] ?? "";
         $data[$k]['is_valid']         = $v['is_valid'] ?? 1;                                          // 不传默认为有效
         $data[$k]['created_at']       = nowTime();
